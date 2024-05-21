51521e4c0e3c738d1d4e97c2e1d932a50e96b714
Melissauribe636 <melissa.uribe636@pascualbravo.edu.co>
2024-05-21 17:31:14 -0500

    Actualizacion cursos

--- src/datos/cursos.js
+++ src/datos/cursos.js
@@ -1,7 +1,41 @@
 export const cursos = [
-    { id: 1, titulo: 'Curso 1', descripcion: 'Descripción del curso 1' },
-    { id: 2, titulo: 'Curso 2', descripcion: 'Descripción del curso 2' },
-    { id: 3, titulo: 'Curso 3', descripcion: 'Descripción del curso 3' },
-    // Añadir más cursos según sea necesario
-  ];
-  
+  {
+    id: 1,
+    titulo: 'Curso de React',
+    descripcion: 'Aprende los fundamentos de React.',
+    progreso: 0, // Progreso inicial en 0%
+    preguntas: [
+      {
+        pregunta: '¿Qué es React?',
+        opciones: [
+          'Una biblioteca de JavaScript',
+          'Un framework de CSS',
+          'Un lenguaje de programación',
+          'Una base de datos'
+        ],
+        respuestaCorrecta: 'Una biblioteca de JavaScript'
+      },
+      // Agrega más preguntas según sea necesario
+    ]
+  },
+  {
+    id: 2,
+    titulo: 'Curso de JavaScript',
+    descripcion: 'Domina JavaScript desde cero.',
+    progreso: 0, // Progreso inicial en 0%
+    preguntas: [
+      {
+        pregunta: '¿Qué es JavaScript?',
+        opciones: [
+          'Un lenguaje de marcado',
+          'Un lenguaje de programación',
+          'Un sistema operativo',
+          'Una herramienta de diseño gráfico'
+        ],
+        respuestaCorrecta: 'Un lenguaje de programación'
+      },
+      // Agrega más preguntas según sea necesario
+    ]
+  },
+  // Agrega más cursos según sea necesario
+];

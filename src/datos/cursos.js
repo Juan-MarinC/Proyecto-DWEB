59f20756724d8cd119458429eb4d680cd2a233c1
SebasValenciaV <sebas1234989252@gmail.com>
2024-05-25 21:26:53 -0500

    + cursos

--- src/datos/cursos.js
+++ src/datos/cursos.js
@@ -115,5 +115,178 @@ export const cursos = [
       }
     ]
   },
-  // Agrega más cursos según sea necesario
+  {
+    id: 3,
+    titulo: 'Curso de HTML y CSS',
+    descripcion: 'Aprende los fundamentos del diseño y estructura de páginas web.',
+    progreso: 0, // Progreso inicial en 0%
+    preguntas: [
+      {
+        pregunta: '¿Qué significa HTML?',
+        opciones: [
+          'HyperText Markup Language',
+          'Home Tool Markup Language',
+          'Hyperlinking Text Marking Language',
+          'Hyperlink Tool Management Language'
+        ],
+        respuestaCorrecta: 'HyperText Markup Language'
+      },
+      {
+        pregunta: '¿Qué es CSS?',
+        opciones: [
+          'Cascading Style Sheets',
+          'Creative Style System',
+          'Cascading System Sheets',
+          'Creative Sheets Style'
+        ],
+        respuestaCorrecta: 'Cascading Style Sheets'
+      },
+      {
+        pregunta: '¿Cuál es la función de la etiqueta <div> en HTML?',
+        opciones: [
+          'Definir una división o sección en un documento',
+          'Crear un enlace',
+          'Incluir una imagen',
+          'Establecer un formulario'
+        ],
+        respuestaCorrecta: 'Definir una división o sección en un documento'
+      },
+      {
+        pregunta: '¿Qué propiedad de CSS se usa para cambiar el color de fondo de un elemento?',
+        opciones: [
+          'background-color',
+          'color',
+          'bgcolor',
+          'background'
+        ],
+        respuestaCorrecta: 'background-color'
+      },
+      {
+        pregunta: '¿Qué es el modelo de caja en CSS?',
+        opciones: [
+          'Un diseño de cuadrícula',
+          'Un modelo que define el espacio de diseño y el comportamiento de los elementos',
+          'Una caja de herramientas',
+          'Un método de depuración'
+        ],
+        respuestaCorrecta: 'Un modelo que define el espacio de diseño y el comportamiento de los elementos'
+      }
+    ]
+  },
+  {
+    id: 4,
+    titulo: 'Curso de Node.js',
+    descripcion: 'Domina el entorno de ejecución de JavaScript del lado del servidor.',
+    progreso: 0, // Progreso inicial en 0%
+    preguntas: [
+      {
+        pregunta: '¿Qué es Node.js?',
+        opciones: [
+          'Un entorno de ejecución de JavaScript en el lado del servidor',
+          'Un framework de JavaScript',
+          'Un lenguaje de programación',
+          'Una base de datos'
+        ],
+        respuestaCorrecta: 'Un entorno de ejecución de JavaScript en el lado del servidor'
+      },
+      {
+        pregunta: '¿Qué gestor de paquetes se utiliza con Node.js?',
+        opciones: [
+          'npm',
+          'pip',
+          'composer',
+          'gem'
+        ],
+        respuestaCorrecta: 'npm'
+      },
+      {
+        pregunta: '¿Cuál de los siguientes es un módulo integrado en Node.js?',
+        opciones: [
+          'express',
+          'react',
+          'http',
+          'lodash'
+        ],
+        respuestaCorrecta: 'http'
+      },
+      {
+        pregunta: '¿Qué es Express.js?',
+        opciones: [
+          'Un framework web para Node.js',
+          'Una base de datos',
+          'Una biblioteca de front-end',
+          'Un gestor de paquetes'
+        ],
+        respuestaCorrecta: 'Un framework web para Node.js'
+      },
+      {
+        pregunta: '¿Qué comando se usa para iniciar un proyecto de Node.js?',
+        opciones: [
+          'node init',
+          'npm start',
+          'npm init',
+          'node start'
+        ],
+        respuestaCorrecta: 'npm init'
+      }
+    ]
+  },
+  {
+    id: 5,
+    titulo: 'Curso de Angular',
+    descripcion: 'Aprende a desarrollar aplicaciones web con el framework Angular.',
+    progreso: 0, // Progreso inicial en 0%
+    preguntas: [
+      {
+        pregunta: '¿Qué es Angular?',
+        opciones: [
+          'Un framework de JavaScript para construir aplicaciones web',
+          'Una biblioteca de JavaScript para el manejo del DOM',
+          'Un lenguaje de programación',
+          'Una base de datos'
+        ],
+        respuestaCorrecta: 'Un framework de JavaScript para construir aplicaciones web'
+      },
+      {
+        pregunta: '¿Qué lenguaje se utiliza principalmente con Angular?',
+        opciones: [
+          'Java',
+          'C++',
+          'TypeScript',
+          'Python'
+        ],
+        respuestaCorrecta: 'TypeScript'
+      },
+      {
+        pregunta: '¿Qué es un componente en Angular?',
+        opciones: [
+          'Un tipo de archivo',
+          'Una función que retorna un elemento de Angular',
+          'Una clase con una plantilla y lógica asociada',
+          'Un módulo de CSS'
+        ],
+        respuestaCorrecta: 'Una clase con una plantilla y lógica asociada'
+      },
+      {
+        pregunta: '¿Qué es un servicio en Angular?',
+        opciones: [
+          'Una clase para encapsular lógica de negocio y comunicación con el servidor',
+          'Un archivo de configuración',
+          'Un componente visual',
+          'Un estilo de diseño'
+        ],
+        respuestaCorrecta: 'Una clase para encapsular lógica de negocio y comunicación con el servidor'
+      },
+      {
+        pregunta: '¿Qué comando se usa para crear un nuevo proyecto de Angular?',
+        opciones: [
+          'ng new',
+          'npm start',
+          'node init',
+          'angular init'
+        ],
+        respuestaCorrecta: 'ng new'
+      }
+    ]
+  },
 ];
